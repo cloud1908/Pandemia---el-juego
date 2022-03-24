97e038e9743a09bfa60618b884497737afeb583d
HTtécnico <75030096+cloud1908@users.noreply.github.com>
2022-03-24 00:44:57 -0300

    Add files via upload

---
+++ Resources/Scripts/LunaTechs - Kino/LunaCopyrightParams.js
@@ -0,0 +1,66 @@
+/*:
+@author LunaTechs - Kino
+@plugindesc A plugin that displays the copyright
+text on the title screen <LunaCopyright>.
+
+@target MV MZ
+
+@param x
+@text X Position
+@desc The X position of the copyright window.
+@default 680
+
+@param y
+@text Y Position
+@desc The Y position of the copyright window.
+@default 570
+
+@param width
+@text Width
+@desc The width of the copyright window.
+@default 170
+
+@param height
+@text Height
+@desc The height of the copyright window
+@default 75
+
+@param fontSize
+@text Font Size
+@desc The font size of the copyright window
+@default 28
+
+@param fontFace
+@text Font Face
+@desc The font face of the copyright window optional parameter(requires Luna-More-Fonts);
+
+@param copyrightText
+@text Copyright Text
+@desc The copyright text 
+@default © LunaTechs 2020-2021
+
+@help
+A plugin that displays the current build version of your
+project on the title screen.
+
+
+MIT License
+Copyright (c) 2020 LunaTechsDev
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+*/
+
+
